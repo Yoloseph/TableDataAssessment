27ee704b109b68636ef7e37959c69b8a280ec298
Jose Baroa <zebaroa@gmail.com>
2024-10-18 00:38:23 +0100

    Adding mock data Getting data to dropdowns

--- table-assessment/src/App.jsx
+++ table-assessment/src/App.jsx
@@ -1,18 +1,34 @@
+import { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Table from "./components/Table"
-import Filter from "./components/Filter"
-
+import Table from "./components/Table";
+import Filter from "./components/Filter";
+import data from "./mock/mockData.json";
 
 function App() {
+  const [tableData, setTableData] = useState([]);
+  const [filterData, setFilterData] = useState([]);
+
+  useEffect(() => {
+    retrieveData();
+  }, [tableData, filterData]);
+
+  //ToDo Make it assynchronous
+  const retrieveData = () => {
+    if (data) {
+      setTableData(data.values);
+      setFilterData(data.dimensions);
+    }
+  };
+
   return (
     <div className="App">
       <h1>Table Data</h1>
 
       <p>Filtering area</p>
-      {<Filter/>}
+      {<Filter data={filterData} />}
       <p>Table area</p>
-      {<Table/>}
+      {<Table data={tableData}/>}
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- table-assessment/src/components/Filter.jsx
+++ table-assessment/src/components/Filter.jsx
@@ -1,13 +1,41 @@
-function Filter() {
+import { useEffect, useState } from "react";
 
-    
+function Filter(data) {
+  //const [columnValue, setTableData] = useState([]);
+
+  useEffect(() => {
+    if (data) {
+    }
+  }, []);
+
+  const columnDataBlock = (data) => {
+    let option = [];
+    if (data) {
+      Object.keys(data.data).forEach((element) => {
+        option.push(<option value="element">{element}</option>);
+      });
+    }
+    return <select>{option}</select>;
+  };
+
+  const valuesDataBlock = (data) => {
+  //ToDo 
+  };
 
   return (
     <div>
       <h1>Filter </h1>
+      <p>Column</p>
+      {columnDataBlock(data)}
+      {/* <select>
+        <option value="someOption">Column option</option>
+        <option value="otherOption">Other Column option</option>
+      </select> */}
+
+      <p>Value</p>
       <select>
-        <option value="someOption">Some option</option>
-        <option value="otherOption">Other option</option>
+        <option value="someOption">Value option</option>
+        <option value="otherOption">Other Value option</option>
       </select>
     </div>
   );
