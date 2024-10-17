47484bd9c1a8d94d58958873cb78c03ea21ba279
Jose Baroa <zebaroa@gmail.com>
2024-10-17 23:59:41 +0100

    - Project creation - inicial components and structure

---
+++ table-assessment/src/components/Filter.jsx
@@ -0,0 +1,16 @@
+function Filter() {
+
+    
+
+  return (
+    <div>
+      <h1>Filter </h1>
+      <select>
+        <option value="someOption">Some option</option>
+        <option value="otherOption">Other option</option>
+      </select>
+    </div>
+  );
+}
+
+export default Filter;
