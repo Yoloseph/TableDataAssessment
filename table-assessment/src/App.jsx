256fa9cf78f57453a3b6148875038f1eb9b444ad
Jose Baroa <zebaroa@gmail.com>
2024-10-19 00:25:05 +0100

    Adding Axios to project Calling mock locally with Axios Changing mocks files location

--- table-assessment/src/App.jsx
+++ table-assessment/src/App.jsx
@@ -2,30 +2,29 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import Table from "./components/Table";
 import Filter from "./components/Filter";
-import dataTable from "./mock/mockDataTable.json";
-import dataUsers from "./mock/mockDataUsers.json";
+import axios from "axios";
 
 function App() {
   const [tableData, setTableData] = useState([]);
   const [userData, setUserData] = useState([]);
 
   useEffect(() => {
-    retrieveData();
-  }, [tableData, userData]);
-
-  //ToDo Make it assynchronous
-  const retrieveData = () => {
-    if (dataTable && dataUsers) {
-      setTableData(dataTable.header);
-      setUserData(dataUsers);
-    }
-  };
+    Promise.all([
+      axios.get("/mock/mockDataTable.json"),
+      axios.get("/mock/mockDataUsers.json"),
+    ])
+      .then(([tableResponse, userResponse]) => {
+        setTableData(tableResponse.data.header);
+        setUserData(userResponse.data);
+      })
+      .catch((error) => {
+        console.error("Error loading the JSON files:", error);
+      });
+  }, []);
 
   const handleFilter = (data) => {
-    if(data.length >= 2){
-
-    }else{
-
+    if (data.length >= 2) {
+    } else {
     }
     console.log("recieved data =>", data);
   };
